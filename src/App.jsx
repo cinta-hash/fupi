12488fc70c251a915f41f2c582bd4e060f9d3232
jacinta <jacintawmwangi21@gmail.com>
2024-01-24 08:54:41 +0300

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import About from './About'
 import './App.css'
+import Home from './home'
 
 function App() {
 
   return (
     <>
       <h1>hello world</h1>
+      <Home />
+      <About />
     </>
   )
 }
